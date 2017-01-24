2cd92a84afd1f29a63ffcdf76a60e42156153f37
rackstar17 <rackstar17@gmail.com>
2017-01-24 20:08:25 +0530

    Added api to get attendance page

--- webkiosk_request.js
+++ webkiosk_request.js
@@ -1,51 +1,77 @@
 var request = require('request');
 var querystring = require('querystring');
+var restify = require('restify');
+var bodyParser=require('body-parser');
+var morgan=require('morgan');
+var server = restify.createServer();
+
+server.use(bodyParser.urlencoded({extended:true}));
+server.use(bodyParser.json());
+server.use(morgan('dev'));
+
 var form = {
   x: "",
   txtInst: "Institute",
-  InstCode: "JIIT",
+  InstCode: "",
   txtuType: "Member Type",
   UserType: "S",
   txtCode: "Enrollment No",
-  MemberCode: "13103687",
+  MemberCode: "",
   txtPin: "Password/Pin",
-  Password: "017687IM",
-  BTNSubmit: "Submit",
-  DOB: "DOB",
-  DATE1: "10-12-1995"
+  Password: "",
+  BTNSubmit: "Submit"
 }
-var cookieJar = request.jar(),
-    formData = querystring.stringify(form),
-    loginURL = 'https://webkiosk.jiit.ac.in/CommonFiles/UserAction.jsp?' + formData;
+var cookieJar = request.jar();
 
-console.log(loginURL);
+// function to login to webkiosk web app
+function webkioskLogin (enroll, password, institute, returnResponse, callback) {  
+  form.InstCode = institute;
+  form.MemberCode = enroll;
+  form.Password = password;
 
-request({
-  method: 'GET',
-  url: loginURL,
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Connection': 'keep-alive'
-  },
-  jar: cookieJar
-}, function (err, res, body) {
-    if(!err & res.statusCode == 200) {
-      request({
-        method: 'GET',
-        url: 'https://webkiosk.jiit.ac.in/StudentFiles/FrameLeftStudent.jsp',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'Connection': 'keep-alive'
-        },
-        jar: cookieJar
-      }, function (err, res, body) {
-          
-      })
-    }
-})
+  var formData = querystring.stringify(form),
+      loginURL = 'https://webkiosk.jiit.ac.in/CommonFiles/UserAction.jsp?' + formData;
 
+  // https request to login to the webkiosk
+  request({
+    method: 'GET',
+    url: loginURL,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Connection': 'keep-alive'
+    },
+    jar: cookieJar
+  }, function (err, res, body) {
+      if(!err & res.statusCode == 200) {
+        console.log('login successfull');
+        callback(returnResponse);
+      }
+  })
+}
 
+// function to get the overall attendance 
+function getOverallAttendance (returnResponse) {
+  // https get request to get the Attendance page to crawl
+  request({
+    method: 'GET',
+    url: 'https://webkiosk.jiit.ac.in/StudentFiles/Academic/StudentAttendanceList.jsp',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Connection': 'keep-alive'
+    },
+    jar: cookieJar
+  }, function (err, res, body) {
+      returnResponse.send(body);
+  });
+}
 
+// API Routes
 
+server.post('/attendance', function (req, res) {
+  webkioskLogin(req.body.enroll, req.body.password, req.body.institute, res, getOverallAttendance);
+});
 
-
+// Server started at port 8080
+server.listen(8080, function () {
+  console.log('listening at port 8080');
+});
